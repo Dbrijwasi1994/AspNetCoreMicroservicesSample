4cf7dc1af408d249adeaf901a7e648c5b3a34ab6
Deepak Brijwasi <deepak.brijwasi@nagarro.com>
2022-04-11 00:30:07 +0530

    1. Added model classes for Catalog Service. 2. Built product repositories for data access. 3. Built Data Access Layer for Catalog Service. 4. Added controller logic for retrieving and updating the product catalog.

---
+++ src/Services/CatalogService/Catalog.Service/Data/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Service.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Service.Data
+{
+	public interface ICatalogContext
+	{
+		IMongoCollection<Product> Products { get; }	
+	}
+}
