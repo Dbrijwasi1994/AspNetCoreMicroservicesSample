24a95867cc481b3163451f71bbd33a9421351a17
Deepak Brijwasi <dbrijwasi@gmail.com>
2022-04-20 14:37:02 +0530

    Implemented all logic of Ordering service and updated docker yaml files.

--- src/Services/OrderingService/Ordering.Service/Program.cs
+++ src/Services/OrderingService/Ordering.Service/Program.cs
@@ -1,4 +1,7 @@
 using Ordering.Application;
+using Ordering.Infrastructure.Extensions;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Service.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,12 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.MigrateDatabase<OrderContext>((context, services) =>
+{
+    var logger = services.GetService<ILogger<OrderContextSeed>>();
+    OrderContextSeed.SeedAsync(context, logger).Wait();
+}).Run();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
